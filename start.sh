12db980001e8325b2115719556604f5d68fdce1a
Junior Balcazar - Judev <junior.marketing@selk.es>
2025-03-18 15:56:20 +0100

    fix: Se corrige comando de inicio de la aplicacion

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ npx puppeteer browsers install chrome
 # Configurar la ruta al ejecutable de Chrome
 export PUPPETEER_EXECUTABLE_PATH=$(node -e "console.log(require('puppeteer').executablePath())")
 # Iniciar la aplicación
-yarn server.js
+yarn start
+
+
